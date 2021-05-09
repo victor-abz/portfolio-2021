7213b6dcf3ab5f457811489424198c7f18fce004
Abizeyimana Victor <svicky.shema@gmail.com>
2021-05-09 15:36:45 +0200

    Remove unused sidebar

--- src/components/Layout/MenuItem.js
+++ src/components/Layout/MenuItem.js
@@ -1,9 +1,9 @@
 const MenuItem = ({ menu }) => {
-	const { name, label, Icon } = menu;
+	const { url, label, Icon } = menu;
 	return (
 		<li className="my-px">
 			<a
-				href="#"
+				href={url}
 				className="flex flex-row items-center h-10 px-3 rounded-lg text-gray-600  dark:text-gray-300 hover:bg-primary hover:text-white"
 			>
 				<span className="flex items-center justify-center text-lg text-gray-600 dark:text-gray-300">
